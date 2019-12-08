f23b316755d6b2c31be7cc3b7ee2d03f4999b0a1
Blaise <blaise.lengrand@gmail.com>
2019-12-08 20:43:16 +0100

    Adding python version of bridge

---
+++ bridge.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-15 -*-
+
+import os
+import json
+import re
+import subprocess
+from datetime import datetime
+
+# Note: the followign should be added to ~/.ssh/config
+# in order to prevent disconnection.
+# Host *
+# ServerAliveInterval 120
+#
+# Also to access the server from external IP, use the followng option in
+# /etc/ssh/sshd_config
+# GatewayPorts yes
+
+CURRENT_DIRECTORY_PATH = os.path.realpath(os.path.dirname(__file__))
+PATH_LOG = os.path.join(CURRENT_DIRECTORY_PATH, "bridge.log")
+PATH_CONFIG = os.path.join(CURRENT_DIRECTORY_PATH, ".bridge.json")
+
+"""
+Execute process
+"""
+def shell(command):
+	proc = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	(stdout, stderr) = proc.communicate()
+	exitCode = proc.returncode
+	if exitCode != 0:
+		raise Exception(stderr)
+	return stdout
+
+"""
+Add entry to the log
+"""
+def logPrint(action, message):
+	entry = "[%s] [%s] %s" % (str(datetime.now()), action.upper(), message)
+	entry = entry.replace("\n", " ").replace("\r", "")
+	with open(PATH_LOG, "a") as f:
+		f.write(entry + "\n")
+	print(entry)
+
+if __name__ == '__main__':
+
+	config = {
+		"ports": [
+			# List of ports to be forwarded.
+			# Should be written either as a list of port numbers or a string delimited
+			# by ":" specifiyng the port mapping.
+		],
+		# Authentication used for the SSH command
+		"auth": "admin@myserver.com"
+	}
+
+	# Load the configuration
+	if not os.path.exists(PATH_CONFIG):
+		with open(PATH_CONFIG, "w") as f:
+			f.write(json.dumps(config, sort_keys=True, indent=4, separators=(',', ': ')))
+	with open(PATH_CONFIG, "r") as f:
+		configUser = json.load(f)
+		config.update(configUser)
+
+	# Generate the ssh command to connect to the target
+	command = ["ssh", "-nNT", str(config["auth"])]
+	for port in config["ports"]:
+		portList = str(port).split(":")
+		assert len(portList) <= 2, "Wrong format for port '%s'" % (str(port))
+		srcPort = portList[0]
+		dstPort = portList[1] if len(portList) == 2 else portList[0]
+		command += ["-R", "{}:localhost:{}".format(srcPort, dstPort)]
+
+	# Check if process is running
+	processList = shell(["ps", "-eo", "pid,command"])
+	regexpr = r"ssh\s+-nNT\s+" + re.escape(config["auth"]) + r"\b"
+	runningList = []
+	for process in processList.splitlines():
+		if re.search(regexpr, process, re.IGNORECASE):
+			pid = process.strip().split()[0]
+			runningList.append(pid)
+
+	# If running
+	if len(runningList) > 0:
+
+		# Check if the tunnel is still responsive
+		connectionActive = False
+		openPortList = shell(["ssh", str(config["auth"]), "netstat", "-an"])
+		regexpr = r"tcp.*:\b" + re.escape(str(config["ports"]).split(":")[0]) + r"\b.*LISTEN"
+		for openPort in openPortList.splitlines():
+			if re.search(regexpr, openPort, re.IGNORECASE):
+				connectionActive = True
+				break
+
+		# If broken, kill it
+		if not connectionActive:
+			logPrint("broken", "Unresponsive tunnel, killing it")
+			for pid in runningList:
+				shell(["kill", "-9", str(pid)])
+			runningList = []
+
+	# If nothing is running
+	if len(runningList) == 0:
+		logPrint("bridge", "Creating bridge on port(s) %s" % (", ".join([str(port) for port in config["ports"]])))
+		try:
+			shell(command)
+		except Exception as e:
+			logPrint("error", "Failed while creating bridge with error: %s" % (str(e)))
