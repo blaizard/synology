a7aa237572491ff2ee2ac4e76b7988fe870c25a0
Blaise <blaise.lengrand@gmail.com>
2019-12-20 10:27:42 +0100

    Make ssh non-blocking

--- bridge.py
+++ bridge.py
@@ -5,6 +5,7 @@ import os
 import json
 import re
 import subprocess
+import time
 from datetime import datetime
 
 # Note: the followign should be added to ~/.ssh/config
@@ -23,13 +24,18 @@ PATH_CONFIG = os.path.join(CURRENT_DIRECTORY_PATH, ".bridge.json")
 """
 Execute process
 """
-def shell(command):
+def shell(command, noWait=False):
 	proc = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	(stdout, stderr) = proc.communicate()
-	exitCode = proc.returncode
-	if exitCode != 0:
-		raise Exception(stderr)
-	return stdout
+	if not noWait:
+		(stdout, stderr) = proc.communicate()
+		exitCode = proc.returncode
+		if exitCode != 0:
+			raise Exception(stderr)
+		return stdout
+	else:
+		time. sleep(1)
+
+	return ""
 
 """
 Add entry to the log
@@ -53,55 +59,62 @@ if __name__ == '__main__':
 		"auth": "admin@myserver.com"
 	}
 
-	# Load the configuration
-	if not os.path.exists(PATH_CONFIG):
-		with open(PATH_CONFIG, "w") as f:
-			f.write(json.dumps(config, sort_keys=True, indent=4, separators=(',', ': ')))
-	with open(PATH_CONFIG, "r") as f:
-		configUser = json.load(f)
-		config.update(configUser)
-
-	# Generate the ssh command to connect to the target
-	command = ["ssh", "-nNT", str(config["auth"])]
-	for port in config["ports"]:
-		portList = str(port).split(":")
-		assert len(portList) <= 2, "Wrong format for port '%s'" % (str(port))
-		srcPort = portList[0]
-		dstPort = portList[1] if len(portList) == 2 else portList[0]
-		command += ["-R", "{}:localhost:{}".format(srcPort, dstPort)]
-
-	# Check if process is running
-	processList = shell(["ps", "-eo", "pid,command"])
-	regexpr = r"ssh\s+-nNT\s+" + re.escape(config["auth"]) + r"\b"
-	runningList = []
-	for process in processList.splitlines():
-		if re.search(regexpr, process, re.IGNORECASE):
-			pid = process.strip().split()[0]
-			runningList.append(pid)
-
-	# If running
-	if len(runningList) > 0:
-
-		# Check if the tunnel is still responsive
-		connectionActive = False
-		openPortList = shell(["ssh", str(config["auth"]), "netstat", "-an"])
-		regexpr = r"tcp.*:\b" + re.escape(str(config["ports"]).split(":")[0]) + r"\b.*LISTEN"
-		for openPort in openPortList.splitlines():
-			if re.search(regexpr, openPort, re.IGNORECASE):
-				connectionActive = True
-				break
-
-		# If broken, kill it
-		if not connectionActive:
-			logPrint("broken", "Unresponsive tunnel, killing it")
-			for pid in runningList:
-				shell(["kill", "-9", str(pid)])
-			runningList = []
-
-	# If nothing is running
-	if len(runningList) == 0:
-		logPrint("bridge", "Creating bridge on port(s) %s" % (", ".join([str(port) for port in config["ports"]])))
-		try:
-			shell(command)
-		except Exception as e:
-			logPrint("error", "Failed while creating bridge with error: %s" % (str(e)))
+	try:
+	
+		# Load the configuration
+		if not os.path.exists(PATH_CONFIG):
+			with open(PATH_CONFIG, "w") as f:
+				f.write(json.dumps(config, sort_keys=True, indent=4, separators=(',', ': ')))
+		with open(PATH_CONFIG, "r") as f:
+			configUser = json.load(f)
+			config.update(configUser)
+	
+		# Generate the ssh command to connect to the target
+		command = ["ssh", "-nfNT"] + config["auth"]
+		for port in config["ports"]:
+			portList = str(port).split(":")
+			assert len(portList) <= 2, "Wrong format for port '%s'" % (str(port))
+			srcPort = portList[0]
+			dstPort = portList[1] if len(portList) == 2 else portList[0]
+			command += ["-R", "{}:localhost:{}".format(srcPort, dstPort)]
+	
+		# Check if process is running
+		processList = shell(["ps", "-eo", "pid,command"])
+		regexpr = r"ssh\s+-nfNT\s+" + r"\s+".join([re.escape(str(cmd)) for cmd in config["auth"]]) + r"\b"
+		runningList = []
+		for process in processList.splitlines():
+			if re.search(regexpr, process, re.IGNORECASE):
+				pid = process.strip().split()[0]
+				runningList.append(pid)
+	
+		# If running
+		if len(runningList) > 0:
+	
+			# Check if the tunnel is still responsive
+			openPortList = shell(["ssh"] + config["auth"] + ["netstat", "-an"])
+			nbConnectionsActive = 0
+			for port in config["ports"]:
+				regexpr = r"tcp.*:\b" + re.escape(str(port).split(":")[0]) + r"\b.*LISTEN"
+				for openPort in openPortList.splitlines():
+					if re.search(regexpr, openPort, re.IGNORECASE):
+						nbConnectionsActive += 1
+						break
+			connectionActive = True if nbConnectionsActive == len(config["ports"]) else False
+	
+			# If broken, kill it
+			if not connectionActive:
+				logPrint("broken", "Unresponsive tunnel, killing it")
+				for pid in runningList:
+					shell(["kill", "-9", str(pid)])
+				runningList = []
+	
+		# If nothing is running
+		if len(runningList) == 0:
+			logPrint("bridge", "Creating bridge on port(s) %s" % (", ".join([str(port) for port in config["ports"]])))
+			try:
+				shell(command, noWait=True)
+			except Exception as e:
+				logPrint("error", "Failed while creating bridge with error: %s" % (str(e)))
+				
+	except Exception as e:
+		logPrint("error", "Unexpected error: %s" % (str(e)))
