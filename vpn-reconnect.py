9b2b61ee0cf7938f4b4308b2bc49bb16b5b7f324
blaizard <blaise.lengrand@gmail.com>
2023-01-08 11:21:23 +0100

    Adding reconnect script

--- vpn-reconnect.py
+++ vpn-reconnect.py
@@ -1,88 +1,28 @@
-#!/usr/bin/python
-# -*- coding: iso-8859-15 -*-
+#!/usr/bin/python3
 
-import os
-import json
-import re
+import pathlib
 import subprocess
-import time
-from datetime import datetime
+import sys
+import http.client as httplib
 
-CURRENT_DIRECTORY_PATH = os.path.realpath(os.path.dirname(__file__))
-PATH_LOG = os.path.join(CURRENT_DIRECTORY_PATH, "vpn-reconnect.log")
-PATH_CONFIG = os.path.join(CURRENT_DIRECTORY_PATH, ".vpn-reconnect.json")
+scriptDirectoryPath = pathlib.Path(__file__).parent.resolve()
 
-"""
-Execute process
-"""
-def shell(command, noWait=False):
-	proc = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	if noWait:
-		time. sleep(2)
-
-	if proc.poll() or noWait == False:
-		(stdout, stderr) = proc.communicate()
-		exitCode = proc.returncode
-		if exitCode != 0:
-			raise Exception(stderr)
-		return stdout.decode()
-
-	return ""
-
-"""
-Add entry to the log
-"""
-def logPrint(action, message):
-	entry = "[%s] [%s] %s" % (str(datetime.now()), action.upper(), message)
-	entry = entry.replace("\n", " ").replace("\r", "")
-	with open(PATH_LOG, "a") as f:
-		f.write(entry + "\n")
-	print(entry)
+def isInternet() -> bool:
+	conn = httplib.HTTPSConnection("8.8.8.8", timeout=5)
+	try:
+		conn.request("HEAD", "/")
+		return True
+	except Exception:
+		return False
+	finally:
+		conn.close()
 
 if __name__ == '__main__':
 
-	config = {
-		# Those identifier are foun with cat /usr/syno/etc/synovpnclient/openvpn/ovpnclient.conf
-		# Id is something like o1234567890123
-		"id": None,
-		"name": None,
-		"protocol": "openvpn"
-	}
+	result = subprocess.run(["/bin/bash", str(scriptDirectoryPath / "reconnect_vpn" / "reconnect-vpn.sh")])
+	# Reconnect failed
+	if result.returncode == 2:
+		if isInternet():
+			subprocess.run(["reboot"])
 
-	try:
-	
-		# Load the configuration
-		if not os.path.exists(PATH_CONFIG):
-			with open(PATH_CONFIG, "w") as f:
-				f.write(json.dumps(config, sort_keys=True, indent=4, separators=(',', ': ')))
-		with open(PATH_CONFIG, "r") as f:
-			configUser = json.load(f)
-			config.update(configUser)
-	
-		# Check if the VPN is active
-		vpnActive = False
-		try:
-			result = shell(["ifconfig", "tun0"])
-			if "00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00" in result:
-				vpnActive = True
-		except:
-			pass
-
-		if not vpnActive:
-			logPrint("info", "VPN inactive, attempt to reconnect...")
-
-			with open("/usr/syno/etc/synovpnclient/vpnc_connecting", "w") as f:
-				f.write("conf_id={}\r\n".format(config["id"]))
-				f.write("conf_name={}\r\n".format(config["name"]))
-				f.write("proto={}\r\n".format(config["protocol"]))
-
-			logPrint("info", config["id"])
-			logPrint("info", config["name"])
-			logPrint("info", config["protocol"])
-
-			shell(["synovpnc", "reconnect", "--protocol={}".format(config["protocol"]), "--name={}".format(config["name"])])
-			logPrint("info", "Reconnection completed")
-				
-	except Exception as e:
-		logPrint("error", "Unexpected error: %s" % (str(e)))
-		raise
+	sys.exit(result.returncode)
